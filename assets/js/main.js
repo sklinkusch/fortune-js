16bf1737a9772a8082364aeab24da713e4465d0e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-11 00:01:48 +0100

    added some new features (more countries, houses etc.)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,38 @@
+
 const fortune = (
   numch = 0,
   partn = "Jane Doe",
   loc = "space",
-  job = "worker",
+  house = "a house",
+  job = "a worker in",
   area = 1
 ) => {
   let child =
     numch === 0 ? "no kids" : numch === 1 ? `${numch} kid` : `${numch} kids`;
-  return `You will be a ${job} in ${loc}, married to ${partn} with ${child} living in a house of ${area} square meters.`;
+  let sqm = 
+    area === 1 ? `${area} square meter` : `${area} square meters`;
+  return `You will be ${job} ${loc}, married to ${partn} with ${child} living in ${house} of ${sqm}.`;
 };
 
 const randomize = MyArray => Math.floor(Math.random() * MyArray.length);
+const randomvalue = (min,max) => Math.floor(Math.random() * (max - min) + min);
 let jobs = [
-  "Doctor",
-  "Drug dealer",
-  "Murderer",
-  "Pimp",
-  "President",
-  "Farmer",
-  "Pope",
-  "CEO",
-  "Clown",
-  "Bank robber"
+  "a doctor in",
+  "a drug dealer in",
+  "a murderer in",
+  "a pimp in",
+  "a prostitute in",
+  "the president of",
+  "the king of",
+  "the queen of",
+  "the emperor of",
+  "the empress of",
+  "the Dalai Lama in",
+  "a farmer in",
+  "the pope in",
+  "a CEO in",
+  "a clown in",
+  "a bank robber in"
 ];
 let geolocs = [
   "North Korea",
@@ -36,24 +47,100 @@ let geolocs = [
   "Vatican City",
   "Australia",
   "Thailand",
-  "China"
+  "China",
+  "Saudi-Arabia",
+  "France"
 ];
 let partners = [
   "a man",
   "a woman",
-  "several men",
-  "several women",
-  "several women and men",
+  "several men at the same time",
+  "several women at the same time",
+  "several women and men at the same time",
   "no one",
-  "your pet",
+  "your dog",
+  "your cat",
+  "your hamster",
+  "your guinea-pig",
+  "your pig",
+  "your cow",
+  "your bull",
+  "your bear",
+  "your turtle",
+  "your bunny",
   "your job"
 ];
-
+let houses = [
+  "a house",
+  "a flat",
+  "a penthouse",
+  "a public restroom",
+  "a palace",
+  "a whorehouse",
+  "a prison cell",
+  "a tent",
+  "a trailer",
+  "a phone box",
+  "a stable"
+];
 let jobnr = randomize(jobs);
 let geonr = randomize(geolocs);
 let partnr = randomize(partners);
+let housenr = randomize(houses);
 let childnr = Math.floor(20 * Math.random());
-let areanr = Math.floor(1000 * Math.random());
+let minimum; 
+let maximum;
+switch(houses[housenr]){
+  case "a house":
+    minimum = 50;
+    maximum = 201;
+    break;
+  case "a flat":
+    minimum = 30;
+    maximum = 101;
+    break;
+  case "a penthouse":
+    minimum = 500;
+    maximum = 1001;
+    break;
+  case "a public restroom":
+    minimum = 10;
+    maximum = 41;
+    break;
+  case "a palace":
+    minimum = 500;
+    maximum = 6001;
+    break;
+  case "a whorehouse":
+    minimum = 50;
+    maximum = 3001;
+    break;
+  case "a prison cell":
+    minimum = 8;
+    maximum = 11;
+    break;
+  case "a tent":
+    minimum = 4;
+    maximum = 21;
+    break;
+  case "a trailer":
+    minimum = 8;
+    maximum = 21;
+    break;
+  case "a phone box":
+    minimum = 1;
+    maximum = 5;
+    break;
+  case "a stable":
+    minimum = 30;
+    maximum = 5000;
+    break;
+  default:
+    minimum = 1;
+    maximum = 1000;
+}
+let areanr = randomvalue(minimum, maximum);
+// let areanr = Math.floor(1000 * Math.random());
 
 // Prompt
 /*let userJob = prompt("Desired Job?", "Doctor");
@@ -70,6 +157,7 @@ document.getElementById("fortune").innerHTML = fortune(
   childnr,
   partners[partnr],
   geolocs[geonr],
+  houses[housenr],
   jobs[jobnr],
   areanr
 );
